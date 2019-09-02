aaca32db6fdd8368d7d8e3de1071aa9a81d3b794
yeyifu <1437552152@qq.com>
2019-09-03 01:32:13 +0800

    提交

--- zhenye/exportFun/standard.js
+++ zhenye/exportFun/standard.js
@@ -1,10 +1,19 @@
+/*
+ * @Description: 
+ * @version: 
+ * @Date: 2019-09-02 23:50:13
+ * @LastEditors: yeyifu
+ * @LastEditTime: 2019-09-03 01:04:27
+ * @Author: yeyifu
+ * @LastModifiedBy: yeyifu
+ */
 let db = require('../conf/conf');
 // 获取基础配置,轮播图
 const baseConfig=(lang)=>{
     let sql1 = `SELECT * FROM baseConfig  where lang=${lang} and isShow=0`;
     let sql2 = `SELECT * FROM Carousel  where lang=${lang} and isShow=0`;
     let sql3 = `SELECT * FROM news  where lang=${lang} and  newstype=1 and isShow=0 limit 3`;
-    let sql4 = `SELECT * FROM products  where lang=${lang} and type=1 and isShow=0 limit 8`;
+    let sql4 = `SELECT * FROM products  where lang=${lang} and category=1 and isShow=0 limit 8`;
     let sql = `${sql1};${sql2};${sql3};${sql4}`
     return new Promise((resolve, reject)=>{
     db.query(sql, function (err, results) {
@@ -23,6 +32,39 @@ const baseConfig=(lang)=>{
  })  
 }
 
+//获取产品图
+const  productList=(lang)=>{
+    let sql = `SELECT * FROM  productConfig   where isShow=0  group by  id`;
+    return new Promise((resolve,reject)=>{
+        db.query(sql, (err, results) => {
+            if (err) {
+             throw err
+            } else {
+              let getData1 = Promise.all(results.map(item => {
+                let sql = `SELECT * FROM products  where isShow=0  and lang=${lang} and  type=${item.id}`;
+                return new Promise((resolve1, reject1) => db.query(sql, (err, respon) => {
+                  if (err) {
+                    reject1(err);
+                  } else {
+                    resolve1({
+                       productArr: respon,
+                       productConfig:item
+                    });
+                  }
+                }));
+              }));
+              getData1.then(function (success) {
+                resolve(success)
+              }).catch(error=>{
+                reject()  
+              })
+            }
+          });
+    })
+ 
+ } 
+
 module.exports = {
-    baseConfig: baseConfig
+    baseConfig: baseConfig,
+    productList:productList
 }
--- zhenye/routes/web.js
+++ zhenye/routes/web.js
@@ -3,94 +3,95 @@
  * @version: 
  * @Date: 2019-08-20 00:29:24
  * @LastEditors: yeyifu
- * @LastEditTime: 2019-08-20 21:45:44
+ * @LastEditTime: 2019-09-03 01:30:09
  * @Author: yeyifu
  * @LastModifiedBy: yeyifu
  */
 const express = require('express');
 const app = express();
 const router = express.Router();
-const { baseConfig,CarouselConfig } = require('../exportFun/standard');
+const {
+  baseConfig,
+  productList
+} = require('../exportFun/standard');
 const i18n = require('i18n');
-let responseData= {};
+let responseData = {};
 
 i18n.configure({
-	locales: [ 'en', 'cn' ],
-	cookie: 'locale',
-	directory: __dirname + '/locales',
-	defaultLocale: 'cn'
+  locales: ['en', 'cn'],
+  cookie: 'locale',
+  directory: __dirname + '/locales',
+  defaultLocale: 'cn'
 });
 app.use(i18n.init);
 /* 
 接口拦截
 */
-const setLang = function(params) {
-	i18n.setLocale(params);
+const setLang = function (params) {
+  i18n.setLocale(params);
 };
 
 // 首页请求
-router.get('/', function(req, res) {
-	res.redirect(302, '/en/index.html');
+router.get('/', function (req, res) {
+  res.redirect(302, '/en/index.html');
 });
 
 //首页请求
-router.get('/:lang/index.html', function(req, res) {
-	setLang(req.params.lang);
-	baseConfig(req.params.lang=='en'?4:5)
-		.then((respon) => {
-      responseData.title="振达官网首页";
-      responseData.lang=req.params.lang;
-      responseData.href='index';
-      responseData.indexData=respon;   
-
-      console.log(responseData)
-
-
-			res.render('index', {
-			  data:responseData
-			});
-		})
-		.catch((error) => {});
+router.get('/:lang/index.html', function (req, res) {
+  setLang(req.params.lang);
+  baseConfig(req.params.lang == 'en' ? 4 : 5)
+    .then((respon) => {
+      responseData.title = "振达官网首页";
+      responseData.lang = req.params.lang;
+      responseData.href = 'index';
+      responseData.indexData = respon;
+      return productList(req.params.lang == 'en' ? 4 : 5)
+    }).then(success => {
+	  responseData.productList = success;
+      res.render('index', {
+        data: responseData
+      })
+    }).catch((error) => {});
 });
 
 //关于我们
-router.get('/:lang/about.html', function(req, res) {
-	setLang(req.params.lang);
-	res.render('about', {
-		title: '公司简介',
-		lang: req.params.lang,
-		href: 'about'
-	});
+router.get('/:lang/about.html', function (req, res) {
+  setLang(req.params.lang);
+  res.render('about', {
+    title: '公司简介',
+    lang: req.params.lang,
+    href: 'about'
+  });
 });
 
 //产品中心
-router.get('/:lang/product.html', function(req, res) {
-	setLang(req.params.lang);
-	res.render('product', {
-		title: '产品列表',
-		lang: req.params.lang,
-		href: 'product'
-	});
+router.get('/:lang/product.html', function (req, res) {
+  setLang(req.params.lang);
+  res.render('product', {
+    title: '产品列表',
+    lang: req.params.lang,
+    href: 'product'
+  });
 });
 
 //新闻中心
-router.get('/:lang/news.html', function(req, res) {
-	setLang(req.params.lang);
-	res.render('news', {
-		title: '新闻列表',
-		lang: req.params.lang,
-		href: 'news'
-	});
+router.get('/:lang/news.html', function (req, res) {
+  setLang(req.params.lang);
+  res.render('news', {
+    title: '新闻列表',
+    lang: req.params.lang,
+    href: 'news'
+  });
 });
 
 //联系我们
-router.get('/:lang/contact.html', function(req, res) {
-	setLang(req.params.lang);
-	res.render('contact', {
-		title: '新闻列表',
-		lang: req.params.lang,
-		href: 'contact'
-	});
+router.get('/:lang/contact.html', function (req, res) {
+  setLang(req.params.lang);
+  res.render('contact', {
+    title: '新闻列表',
+    lang: req.params.lang,
+    href: 'contact'
+  });
 });
 
 module.exports = router;
