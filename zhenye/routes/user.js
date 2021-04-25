eded4e0e256247013f1fb42f790c4f3ab850b9da
yeyifu <1437552152@qq.com>
2021-04-25 23:23:50 +0800

    tijiao

--- zhenye/routes/user.js
+++ zhenye/routes/user.js
@@ -3,7 +3,7 @@
  * @version:
  * @Date: 2019-08-20 00:29:24
  * @LastEditors: yfye
- * @LastEditTime: 2021-04-11 18:54:23
+ * @LastEditTime: 2021-04-25 23:22:56
  * @Author: yeyifu
  * @LastModifiedBy: yeyifu
  */
@@ -84,9 +84,81 @@ router.post("/updateResume", (req, res) => {
   let education = req.body.education;
   let major = req.body.major;
   let email = req.body.email;
+  let remark = req.body.remark;
 
+  if(!name){
+    res.json({
+      msg: "用户名不能为空",
+      code: 0,
+    });return;
+  }
+  if(!sex){
+    res.json({
+      msg: "性别不能为空",
+      code: 0,
+    });return;
+  }
+  if(!briday){
+    res.json({
+      msg: "生日不能为空",
+      code: 0,
+    });return;
+  }
+  if(!NativePlace){
+    res.json({
+      msg: "籍贯不能为空",
+      code: 0,
+    });return;
+  }
+  if(!qqNum){
+    res.json({
+      msg: "QQ不能为空",
+      code: 0,
+    });return;
+  }
+
+  if(!school){
+    res.json({
+      msg: "毕业院校不能为空",
+      code: 0,
+    });return;
+  }
+  if(!education){
+    res.json({
+      msg: "学历不能为空",
+      code: 0,
+    });return;
+  }
+  if(!major){
+    res.json({
+      msg: "专业不能为空",
+      code: 0,
+    });return;
+  }
+  if(!email){
+    res.json({
+      msg: "邮箱不能为空",
+      code: 0,
+    });
+    return;
+  }
+  if(!remark){
+    res.json({
+      msg: "个人介绍不能为空",
+      code: 0,
+    });
+    return;
+  }
+  let re = /^\w+@[a-zA-Z0-9]{2,10}(?:\.[a-z]{2,4}){1,3}$/;
+  if (!re.test(email)) { 
+    res.json({
+      msg: "邮箱格式不正确",
+      code: 0,
+    });
+    return;
+  }
   let sql =
-    "UPDATE pcUser  set name=?,sex=?,briday=?,NativePlace=?,qqNum=?,school=?,education=?,major=?,email=?  where id=?";
+    "UPDATE pcUser  set name=?,sex=?,briday=?,NativePlace=?,qqNum=?,school=?,education=?,major=?,email=?,remark=? where id=?";
   let param = [
     name,
     sex,
@@ -97,6 +169,7 @@ router.post("/updateResume", (req, res) => {
     education,
     major,
     email,
+    remark,
     id,
   ];
   db.query(sql, param, function (err, results) {
